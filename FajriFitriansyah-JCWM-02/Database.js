1fafc9ea0f00462856e2e27f1eb628f86324bdbf
fajrifitriansyah <fajri.fitriansyah02@gmail.com>
2021-02-26 17:07:27 +0700

    Remed Fundamental

---
+++ FajriFitriansyah-JCWM-02/Database.js
@@ -0,0 +1,17 @@
+let dataProducts = [
+    {
+        name: 'Coca-Cola',
+        price: 5000,
+        category: 'drink'
+    },
+    {
+        name: 'Beng-Beng',
+        price: 4500,
+        category: 'food'
+    },
+    {
+        name: 'Fanta',
+        price: 3500,
+        category: 'drink'
+    }
+]
